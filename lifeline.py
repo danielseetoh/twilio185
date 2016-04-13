57a092ef3510a1ff671ae05394998ec519d3d017
danielseetoh <danielseetoh92@gmail.com>
2016-04-13 08:39:38 +0000

    test

--- lifeline.py
+++ lifeline.py
@@ -373,5 +373,6 @@ def acknowledge_requestor(phonenumber):
     )   
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
